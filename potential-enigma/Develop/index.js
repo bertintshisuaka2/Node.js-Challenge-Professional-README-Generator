9f8e72004b029a57f1094427b5e66b60bc78f7b2
bertintshisuaka2 <bertintshisuaka@hotmail.com>
2023-02-11 14:26:17 -0500

    Final Product Commit

---
+++ potential-enigma/Develop/index.js
@@ -0,0 +1,87 @@
+// TODO: Include packages needed for this application
+const generateMarkDownPackage = require('./utils/generateMarkdown');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+    { 
+        type: "input",
+        message: "This program will help you generate a proper README file. To start, enter the Title of your project: ",
+        name: "title",  
+    },
+    { 
+        type: "input",
+        message: "Next, please provide your name, so you can be credited: ",
+        name: "name",  
+    },
+    { 
+        type: "input",
+        message: "Next, provide a good Description of your project: ",
+        name: "description",  
+    },
+    { 
+        type: "input",
+        message: "How do you Install your project?",
+        name: "installation",  
+    },
+    { 
+        type: "input",
+        message: "How would you Use this application?:",
+        name: "usage",  
+    },
+    { 
+        type: "input",
+        message: "How can other people help to Contribute to this project?:",
+        name: "contributing",  
+    },
+    { 
+        type: "input",
+        message: "How can other people help to Test this application?:",
+        name: "tests",  
+    },
+    { 
+        type: "list",
+        message: "Please enter the license used for this repository: ",
+        choices: ["MIT", "GNU 2.0", "GNU 3.0", "Apache License 2.0", "No License"],
+        name: "licenses",  
+    },
+    { 
+        type: "input",
+        message: "Please enter your GitHub username: ", 
+        name: "github",  
+    },
+    { 
+        type: "input",
+        message: "Please enter an email to which people can contact you for questions: ",
+        name: "email",  
+    }
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, generatedMD) {
+    fs.writeFile(fileName, generatedMD, err => 
+        err ? console.log(err) : console.log('Success!'));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+
+    inquirer
+        .prompt(questions)
+        .then(function(data) {
+            console.log(data);
+            const generatedMD = generateMarkDownPackage(data);
+            const fileName = './sample/README.md';
+
+            writeToFile(fileName, generatedMD);
+        });
+
+}
+
+// Function call to initialize app
+init();
+
+//exports
+module.exports = questions;
---
+++ potential-enigma/Develop/utils/generateMarkdown.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const index = require('../index');
+
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  let licenseBadge = '';
+  if(license === 'MIT') {
+    licenseBadge = '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+  }
+  else if (license === 'GNU 2.0') {
+    licenseBadge = '![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)';
+  }
+  else if (license === 'GNU 3.0') {
+    licenseBadge = '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+  }
+  else if (license === 'Apache 2.0') {
+    licenseBadge = '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+  }
+  else {
+    licenseBadge = '';
+  }
+  return licenseBadge;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  let licenseLink = '';
+
+  if (license === 'MIT') {
+    licenseLink = 'https://opensource.org/licenses/MIT';
+  }
+  else if (licenseLink === 'GNU 2.0') {
+    licenseLink = 'https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html';
+  }
+  else if (licenseLink === 'GNU 3.0') {
+    licenseLink = 'https://www.gnu.org/licenses/gpl-3.0';
+  }
+  else if (licenseLink === 'Apache 2.0') {
+    licenseLink = 'https://opensource.org/licenses/Apache-2.0';
+  }
+  else {
+    licenseLink = '';
+  }
+  return licenseLink;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  let theLicenseSection = '';
+
+  if(license === 'No License'){
+    theLicenseSection = '';
+  }
+  else {
+    theLicenseSection = `License: ${license} `;
+  }
+  return theLicenseSection;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return`
+  # ${data.title}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contribution)
+  * [Tests](#tests)
+  * [Licenses](#licenses)
+  * [Credits](#credits)
+  * [Questions](#questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## Usage
+  ${data.usage}
+  ## Contributions
+  ${data.contributing}
+  ## Testing
+  ${data.tests}
+  ## Licenses
+  ${renderLicenseSection(data.licenses)} 
+  ${renderLicenseBadge(data.licenses)}
+  ${renderLicenseLink(data.licenses)}
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
+  ## Credits
+  ${data.name}
+`;
+}  
+
+//export
+module.exports = generateMarkdown;
